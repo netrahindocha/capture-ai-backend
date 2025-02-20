b7c15124cba3af16d7fd0a764147cce074ce07be
netrahindocha <netrahindocha1@gmail.com>
2025-02-19 22:17:20 -0500

    CP-20 Changing OAuth Redirect URI

--- src/configs/passport.js
+++ src/configs/passport.js
@@ -11,7 +11,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${process.env.FRONTEND_BASE_URL}auth/google/callback`,
+      callbackURL: `${process.env.FRONTEND_BASE_URL}/api/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
